86b63d0e4df987dfeeba009eeda7c714c1aa6fa1
morteza <nancologist@gmail.com>
2025-05-08 16:15:54 +0200

    Add car-details UI

--- my-app/src/pages/car-details/car-details.component.ts
+++ my-app/src/pages/car-details/car-details.component.ts
@@ -1,23 +1,45 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, DestroyRef, OnInit} from '@angular/core';
 import {AppService} from '../../app/app.component.service';
 import {Car} from '../../shared';
 import {ActivatedRoute} from '@angular/router';
+import {MatCardModule} from '@angular/material/card';
+import {DatePipe, NgForOf, NgIf} from '@angular/common';
+import {MatListModule} from '@angular/material/list';
+import {MatIcon} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 
 @Component({
-  selector: 'app-car-details',
-  imports: [],
-  templateUrl: './car-details.component.html',
-  styleUrl: './car-details.component.css'
+    selector: 'app-car-details',
+    imports: [
+        MatButtonModule,
+        MatCardModule,
+        MatListModule,
+        MatIcon,
+        DatePipe,
+        NgIf,
+        NgForOf
+    ],
+    templateUrl: './car-details.component.html',
+    styleUrl: './car-details.component.css'
 })
 export class CarDetailsComponent implements OnInit {
-    constructor(private appService: AppService, private route: ActivatedRoute) {
+    car: Car | null = null;
+
+    constructor(
+        private appService: AppService,
+        private route: ActivatedRoute,
+        private destroyRef: DestroyRef
+    ) {
     }
 
     ngOnInit() {
         const carId = +(this.route.snapshot.params as { id: string }).id;
-        this.appService.getCarById(carId).subscribe((car: Car) => {
-            console.log(car)
-            console.log(car.equipments![0])
-        })
+        this.appService
+            .getCarById(carId)
+            .pipe(takeUntilDestroyed(this.destroyRef))
+            .subscribe((car: Car) => {
+                this.car = car;
+            });
     }
 }
