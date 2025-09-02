7c563056d67e104d0e56188857316c489c0a2734
morteza <nancologist@gmail.com>
2025-09-02 11:14:20 +0200

    Update API URL in env file

--- my-app/src/environments/environment.ts
+++ my-app/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://car360-backend.railway.internal/api'
+    apiUrl: 'https://car360-backend-production.up.railway.app/api'
 };
