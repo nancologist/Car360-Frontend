2616b51a44713c047650c3117833f59426856409
morteza <nancologist@gmail.com>
2025-09-04 19:45:45 +0200

    Move loading cars api call to the effects

--- my-app/src/shared/api-types.ts
+++ my-app/src/shared/api-types.ts
@@ -43,6 +43,22 @@ export interface paths {
             path?: never;
             cookie?: never;
         };
+        get: operations["getAllEquipments"];
+        put?: never;
+        post?: never;
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
+    "/api/equipments/search": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
         get: operations["searchEquipments"];
         put?: never;
         post?: never;
@@ -230,6 +246,26 @@ export interface operations {
                 };
                 content: {
                     "*/*": components["schemas"]["LoginResponse"];
+                };
+            };
+        };
+    };
+    getAllEquipments: {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        requestBody?: never;
+        responses: {
+            /** @description OK */
+            200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "*/*": components["schemas"]["EquipmentDto"][];
                 };
             };
         };
